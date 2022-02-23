cfd187f8d4a94abe5497e4beceef2c49a15dad87
Hector GODZIEN <hgodzien@gmail.com>
2022-02-24 00:33:04 +0100

    Updated oxo_result.py

--- oxo_result.py
+++ oxo_result.py
@@ -3,9 +3,22 @@
 
 import random
 
+
 class Board:
-    def __init__(self, height:int, width:int):
+    def __init__(self, height: int, width: int):
         self.__board = [[None for w in range(width)] for h in range(height)]
+        # self.width = width
+        # self.height = height
+
+    # @property
+    # def board(self):
+    #     tmp = []
+    #     for i in range(self.height):
+    #         tmp2 = []
+    #         for j in range(self.width):
+    #             tmp2.append(self.__board[i+j])
+    #         tmp.append(tmp2)
+    #     return tmp
 
     def __str__(self):
         res = ""
@@ -15,15 +28,36 @@ class Board:
                 if indexj == 0:
                     res += f"{indexi }"
                 res += f"| {column} |" if column is not None else "|   |"
-            res += '\n'
+            res += "\n"
         res += "---------------\n"
         return res
-    
-    def add(self, pos:tuple, number):
+
+    def add(self, pos: tuple, number):
+        self.lastAddPosition = pos
         self.__board[pos[0]][pos[1]] = number
-    
+
     def winned(self):
+        # Horizontal
+        if (
+            self.__board[self.lastAddPosition[0]][0] == self.__board[self.lastAddPosition[0]][1]
+            and self.__board[self.lastAddPosition[0]][1] == self.__board[self.lastAddPosition[0]][2]
+        ):
+            return self.__board[self.lastAddPosition[0]][0]
+
+        # Vertical
+        if (
+            self.__board[0][self.lastAddPosition[1]] == self.__board[1][self.lastAddPosition[1]]
+            and self.__board[1][self.lastAddPosition[1]] == self.__board[2][self.lastAddPosition[1]]
+        ):
+            return self.__board[0][self.lastAddPosition[1]]
+
+        # # Diagonal
+        # if self.lastAddPosition[0] != 1 and self.lastAddPosition[1] != 1: # lastAddPosition is either first or last line and either first or last column
+        #     if self.lastAddPosition[0] == 0:
+        #         if self.lastAddPosition[1] == 0 and self.__board[]
+
         return False
+
 
 class Player:
     def __init__(self, nickname, token):
@@ -36,17 +70,18 @@ class Player:
         column = input("Please select column: ")
         return (int(line), int(column)), self.token
 
+
 class Game:
     def __init__(self, player1, player2):
-        self.p1 = Player(player1, 'x')
-        self.p2 = Player(player2, 'o')
-        self.board = Board(3,3)
+        self.p1 = Player(player1, "x")
+        self.p2 = Player(player2, "o")
+        self.board = Board(3, 3)
         self.currentPlayer = 0
         self.finished = False
 
     def __str__(self):
         return self.board.__str__()
-    
+
     def play(self, values):
         self.board.add(*values)
         if self.board.winned():
@@ -62,18 +97,15 @@ class Game:
 # }
 
 
-
 if __name__ == "__main__":
     game = Game("Hector", "Gilles")
 
     PLAYER_TURN = [game.p1.play, game.p2.play]
 
-    game.currentPlayer = random.choice([0,1])
+    game.currentPlayer = random.choice([0, 1])
 
     while not game.finished:
         print(game)
         # game.play(PLAYER_TURN[str(game.currentPlayer)]())
         print(f"game.currentPlayer = {game.currentPlayer}")
         game.play(PLAYER_TURN[game.currentPlayer]())
-
-
